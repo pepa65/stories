23655b6fce1a7a40baf87e3f81532b2395b11ac7
gitlab.com/pepa65 <pepa65@passchier.net>
2024-01-23 22:15:53 +0700

    Yellow border for no audio

---
+++ i.js
@@ -0,0 +1,4 @@
+var img=document.getElementById('img');
+img.setAttribute('border', '3px solid yellow');
+document.addEventListener('keypress', function(){location.href='index.html';});
+document.addEventListener('click', function(){location.href='index.html';});
