ab2f5e494669ddb52b253bb9c34216e1fa4f4c18
gitlab.com/pepa65 <pepa65@passchier.net>
2024-01-24 08:19:48 +0700

    Distinguish no-audio

--- i.js
+++ i.js
@@ -1,4 +1,3 @@
-var img=document.getElementById('img');
-img.setAttribute('border', '3px solid yellow');
+document.getElementById('img').style.border='5px solid red';
 document.addEventListener('keypress', function(){location.href='index.html';});
 document.addEventListener('click', function(){location.href='index.html';});
