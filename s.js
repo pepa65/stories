0cb5bf13ffda0a481672b05992bff2943fb02cc3
gitlab.com/pepa65 <pepa65@passchier.net>
2024-01-26 23:24:21 +0700

    rotate

--- s.js
+++ s.js
@@ -1,12 +1,13 @@
 const SWIPE_MAX_MS=1500;
 const SWIPE_MIN_PX=100;
+const DELAY_MS=500;
 var a=new Audio();
 a.src='%1.mp3';
 var h=0;
 var img=document.getElementById('img');
-document.addEventListener('keypress', function(){location.href='index.html';});
-document.addEventListener('dblclick', function(){location.href='index.html';});
-document.addEventListener('click', function(){if(a.paused) setTimeout('a.play()', 500); else a.pause();});
+document.addEventListener('keypress', function(){location.href='index.html'});
+document.addEventListener('dblclick', function(){location.href='index.html'});
+document.addEventListener('click', function(){if(a.paused) setTimeout(()=>{a.play()}, DELAY_MS); else a.pause()});
 document.addEventListener('touchstart', touchStart);
 document.addEventListener('touchend', touchEnd);
 function touchStart(e){
